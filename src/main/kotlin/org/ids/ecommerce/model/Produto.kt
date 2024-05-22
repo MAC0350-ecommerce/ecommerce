56cf6402b07a91288270b5041665960fd61895e4
Marcelo Nascimento <marcelojunior@usp.br>
2024-05-22 17:53:18 -0300

    test: produtoRepositoryTest

--- src/main/kotlin/org/ids/ecommerce/model/Produto.kt
+++ src/main/kotlin/org/ids/ecommerce/model/Produto.kt
@@ -1,8 +1,11 @@
 package org.ids.ecommerce.model
 
 import jakarta.persistence.*
+import jakarta.validation.constraints.NotBlank
+import jakarta.validation.constraints.NotEmpty
 import org.hibernate.annotations.Generated
 import org.hibernate.annotations.GenerationTime
+import org.ids.ecommerce.repository.ProdutoRepository
 import java.util.*
 
 
@@ -20,6 +23,22 @@ class Produto (
     @Temporal(TemporalType.TIMESTAMP)
     @Column(nullable = false, updatable = false, insertable = false, columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP")
     var dataCadastro: Date?,
-    @OneToMany(cascade = [CascadeType.ALL], fetch = FetchType.LAZY)
+    @field:NotEmpty(message = "Lista de fotos não pode ser vazia")
+    @OneToMany(cascade = [CascadeType.ALL], fetch = FetchType.EAGER)
     var fotos : List<Foto>
-)
+) {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other !is Produto) return false
+        return this.id == other.id &&
+                this.nome == other.nome &&
+                this.ativado == other.ativado &&
+                this.preco == other.preco &&
+                this.dataCadastro == other.dataCadastro &&
+                this.descricao == other.descricao
+    }
+
+    override fun hashCode(): Int {
+        return id ?: 0
+    }
+}
