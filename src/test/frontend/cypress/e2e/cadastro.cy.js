fec38a1ecde1be8e66d7cbc172a72b7f4f7db1ed
victor-nasc <victor_nascimento@usp.br>
2024-05-27 08:37:29 -0300

    add painel final

--- src/test/frontend/cypress/e2e/cadastro.cy.js
+++ src/test/frontend/cypress/e2e/cadastro.cy.js
@@ -5,19 +5,25 @@ Cypress.on('uncaught:exception', (err, runnable) => {
 
 
 describe('Criar conta', () => {
-    it('Test0 - Senhas iguais', () => {
+    beforeEach(() => {
         cy.visit('http://localhost:8080/signup')
+    })
+
+    it('Test0 - Senhas iguais', () => {
         // dados do usuario
         cy.get('#nome').type('test')
         cy.get('#login').type('test')
         cy.get('#senha').type('test123')
         cy.get('#confirmar-senha').type('test123') // senha diferente
         cy.get('.botao-estilo1').click()
-        // redireciona para login
-        cy.url().should('eq', 'http://localhost:8080/login')
+
+        // alerta da conta criada
+        cy.on('window:alert', (text) => {
+            expect(text).to.contains('Conta criada com sucesso!');
+        });
     })
+
     it('Test1 - Senhas diferentes', () => {
-        cy.visit('http://localhost:8080/signup')
         // dados do usuario
         cy.get('#nome').type('test')
         cy.get('#login').type('test')
@@ -25,6 +31,7 @@ describe('Criar conta', () => {
         cy.get('#confirmar-senha').type('test123456789') // senha diferente
         cy.get('.botao-estilo1').click()
         cy.url().should('eq', 'http://localhost:8080/signup')
+
         // erro
         cy.get('.error-message')
             .should('exist')
@@ -35,35 +42,58 @@ describe('Criar conta', () => {
 
 
 describe('Login', () => {
-    // Login senha correta
-    it('Test2 - Login valido', () => {
+    beforeEach(() => {
         cy.visit('http://localhost:8080/login')
+    })
+
+    it('Test2 - Login usuario valido', () => {
         // login
-        cy.get('#login').type('test')
-        cy.get('#senha').type('test')
+        cy.get('#login').type('cliente_teste')
+        cy.get('#senha').type('1234')
         cy.get('#login-button').click()
+        
         // homepage logado
         cy.url().should('eq', 'http://localhost:8080/')
-        cy.get('#nome')
-            .should('exist')
-            .should('have.text', 'test')
+        
         // deslogar
         cy.get('.logout-btn')
             .should('exist')
             .should('have.text', 'Desconectar')
         cy.get('.logout-btn').click()
         cy.url().should('eq', 'http://localhost:8080/')
-        cy.get('#nome').should('not.exist')
         cy.get('.logout-btn').should('not.exist')
     })
-    // Login senhas errada
-    it('Test3 - Login invalido', () => {
+
+
+    it ('Test3 - Login admin', () => {
+        // login
+        cy.get('#login').type('cliente_teste')
+        cy.get('#senha').type('1234')
+        cy.get('#login-button').click()
+
+        // homepage logado
+        cy.url().should('eq', 'http://localhost:8080/')
+
+        // pagina admin
+        cy.get('.adm-btn').should('exist')
+
+        // deslogar
+        cy.get('.logout-btn')
+            .should('exist')
+            .should('have.text', 'Desconectar')
+        cy.get('.logout-btn').click()
+        cy.url().should('eq', 'http://localhost:8080/')
+        cy.get('.logout-btn').should('not.exist')
+    })
+
+    it('Test4 - Login invalido', () => {
         cy.visit('http://localhost:8080/login')
         // login
         cy.get('#login').type('123')
         cy.get('#senha').type('1234')
         cy.get('#login-button').click()
         cy.url().should('eq', 'http://localhost:8080/login')
+        
         // erro: senha errada
         cy.get('.error-message')
             .should('exist')
