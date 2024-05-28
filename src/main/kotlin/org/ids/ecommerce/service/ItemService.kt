5cf2fb88b01d393b6c75c95fd9edfe9e0ca0e1fb
Marcelo Nascimento <marcelojunior@usp.br>
2024-05-27 22:51:50 -0300

    chore: atualiza a quantidade do produto quando o item e criado

--- src/main/kotlin/org/ids/ecommerce/service/ItemService.kt
+++ src/main/kotlin/org/ids/ecommerce/service/ItemService.kt
@@ -4,12 +4,14 @@ import org.ids.ecommerce.dto.ItemReq
 import org.ids.ecommerce.dto.ItemRes
 import org.ids.ecommerce.model.Item
 import org.ids.ecommerce.repository.ItemRepository
+import org.ids.ecommerce.repository.ProdutoRepository
 import org.springframework.stereotype.Service
 
 
 @Service
 class ItemService (
-    private var itemRepository: ItemRepository
+    private var itemRepository: ItemRepository,
+    private var produtoRepository: ProdutoRepository
 ) {
     fun findAll() : List<ItemRes> {
         var lista = itemRepository.findAll()
@@ -33,6 +35,9 @@ class ItemService (
     }
 
     fun criaItem(itemReq: ItemReq) : ItemRes {
+
+        var produto = produtoRepository.findById(itemReq.produto_id).get()
+
         var itemSalvo = itemRepository.save(
             Item(
                 id = null,
@@ -41,6 +46,9 @@ class ItemService (
                 dataCadastro = null
             )
         )
+
+        produto.quantidade = produto.quantidade?.plus(1)
+        produtoRepository.save(produto)
 
         return ItemRes(
             id = itemSalvo.id!!,
